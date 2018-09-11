30bc2d3ea415149d697c2453779efa23a588c606
stenbra <stenbra7@gmail.com>
2018-09-11 10:45:20 +0300

    lottonumrorna kan ej vara dublikat av varandra

--- lotto-test-master/js/script.js
+++ lotto-test-master/js/script.js
@@ -1,10 +1,22 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
 function lottoRad() {
+	arra = [];
+	
 	for (var i=0; i<=6; i++) {
 
 		let lottery = Math.floor(Math.random() * 38)+1;
-	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
+		arra[i]=lottery;
+	 	
+		for(var l=i;l>=0; l--){
+
+			if(arra[i]==arra[l]){
+				arra[i]= Math.floor(Math.random() * 38)+1;
+			}
+			
+		}
+		
+	 	document.getElementById ("lotto_" + i).innerHTML =  arra[i];
 
 	}
 }
